3692694c4e0023848edf9f87ccbbd9df2e5637f7
sofiawillebrand <82705594+sofiawillebrand@users.noreply.github.com>
2023-10-08 11:04:13 +0200

    feat: update tech-info and add project to portfolio

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -1,25 +1,22 @@
 import React from "react";
 
 const Tech = () => {
-  return (
-    <section className="portfolio-section">
-      <div className="container">
-        <div className="heading-container">
-          <h1 className="sections-heading">tech</h1>
-        </div>
-        <p className="tech-text">
-          {" "}
-          HTML, CSS, Flexbox,{" "}
-          <span className="color-2">JavaScript</span>, ES6, JSX,
-          <span className="color-2">React</span>,{" "}
-          <span className="color-2">React Hooks</span>,{" "}
-          <span className="color-2">Redux</span>, Node.js, Mongo DB,
-          Web Accessibly, API:s, mob-programming, pair-programming,
-          Github.{" "}
-        </p>
-      </div>
-    </section>
-  );
+    return (
+        <section className="portfolio-section">
+            <div className="container">
+                <div className="heading-container">
+                    <h1 className="sections-heading">tech</h1>
+                </div>
+                <p className="tech-text">
+                    {" "}
+                    HTML, CSS, Flexbox, <span className="color-2">TypeScript</span>, ES6, JSX,
+                    <span className="color-2">React</span>, <span className="color-2">Svelte</span>, Node.js,{" "}
+                    <span className="color-2">Redux</span>, Responsive design, Web Accessibly, Mob-programming,
+                    Pair-programming, Github.{" "}
+                </p>
+            </div>
+        </section>
+    );
 };
 
 export default Tech;
